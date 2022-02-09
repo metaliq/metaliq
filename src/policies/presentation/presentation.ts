61a576746c6bf19c9605205fe6dbb60b2c4c877f
tstewart <tstewart@klaudhaus.com>
2022-02-09 12:19:34 +1100

    v 0.15.1
    Fix over-aggressive override of top level view.

--- src/policies/presentation/presentation.ts
+++ src/policies/presentation/presentation.ts
@@ -26,7 +26,7 @@ export const metaView = <T, P = any> (view: View<T>): MetaView<T, P> =>
 metaSetups.push(meta => {
   // Default the review method of the top level spec to renderPage if not assigned and this policy has been loaded
   if (!meta.$.parent) {
-    if (!meta.$.spec.publication?.target) {
+    if (!meta.$.spec.publication?.target && !meta.$.spec.view) {
       meta.$.spec.view = metaForm()
     }
     if (meta.$.spec.view) {
