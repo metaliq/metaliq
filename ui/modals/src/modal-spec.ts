762b94fd66ac55ab7f80a62b6d1f3869faa3652a
tstewart <tstewart@klaudhaus.com>
2022-12-21 05:28:04 +1100

    Add modal and modal button classes

--- ui/modals/src/modal-spec.ts
+++ ui/modals/src/modal-spec.ts
@@ -4,17 +4,57 @@ import { ViewResult } from "@metaliq/presentation"
 import { modal } from "./modal-view"
 
 export type ModalInfo = {
+  /**
+   * Main modal title.
+   */
   title?: string
+
+  /**
+   * Modal body display content.
+   */
   body?: ViewResult
+
+  /**
+   * Modal buttons.
+   */
   buttons?: ModalButton[]
+
+  /**
+   * Modal is a progress indicato rather than an interactive prompt.
+   */
   isProgress?: boolean
+
+  /**
+   * Class(es) for the top level modal element.
+   */
+  classes?: string
+
+  /**
+   * Class(es) for the icon element.
+   */
   iconClasses?: string
 }
 
 export type ModalButton = {
+  /**
+   * Main button label.
+   */
   label: string
+
+  /**
+   * Click action update.
+   */
   up: (...params: any[]) => any
+
+  /**
+   * Data parameter for click action update.
+   */
   data?: any
+
+  /**
+   * Additional class(es).
+   */
+  classes?: string
 }
 
 export const modalSpec: MetaSpec<ModalInfo> = {
