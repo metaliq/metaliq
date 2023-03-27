e87b220e384be51b744b851698ccc8cb51c9064b
tstewart <tstewart@klaudhaus.com>
2023-03-27 22:43:00 +1100

    Template API running against package.json.

--- create/template/src/api/api-model.ts
+++ create/template/src/api/api-model.ts
@@ -1,20 +1,31 @@
 import { MetaModel } from "metaliq"
 import { Package, Resolvers } from "../gen/graphql-types"
 import { graphQLServer } from "@metaliq/graphql-server"
+import { readFile, writeFile } from "fs/promises"
 
 export const apiModel: MetaModel<Resolvers> = {
   publicationTarget: graphQLServer(),
   resolvers: {
     Query: {
       async fetchPackage() {
-        const result: Package = {
-          author: "mickey.mouse",
-          name: "My new solution",
-          license: "MIT",
-          version: "1.2.3",
-          description: "Awesome!!!"
+        const json = await readFile("./package.json", "utf8")
+        const pkg = JSON.parse(json) as Package
+        const { author, name, license, version, description, devDependencies, dependencies, peerDependencies } = pkg
+        return { author, name, license, version, description, devDependencies, dependencies, peerDependencies }
+      }
+    },
+    Mutation: {
+      async updatePackage(parent, { pkg }) {
+        const oldJson = await readFile("./package.json", "utf8")
+        const oldPkg = JSON.parse(oldJson) as Package
+        const { author, name, license, version, description, devDependencies, dependencies, peerDependencies } = pkg
+        const newPkg: Package = {
+          ...oldPkg,
+          ...pkg
         }
-        return result
+        const newJson = JSON.stringify(newPkg, null, "  ")
+        await writeFile("./package.json", newJson)
+        return newPkg
       }
     }
   }
