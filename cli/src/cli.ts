515f92fc77f90cd95856a67d31cdaa3faa79d33d
tstewart <tstewart@klaudhaus.com>
2023-09-20 21:22:11 +1000

    v 1.0.0-beta.88

--- cli/src/cli.ts
+++ cli/src/cli.ts
@@ -36,7 +36,7 @@ type ServeOptions = {
 const program = new Command()
 program
   .name("metaliq")
-  .version("1.0.0-beta.87")
+  .version("1.0.0-beta.88")
 
 program
   .command("run [modelName]")
