1497262e997363447d683c64b636dcd3f932d89f
tstewart <tstewart@klaudhaus.com>
2023-11-15 13:50:02 +1100

    v 1.0.0-beta.93 - Add fetch option to PDF viewer

--- cli/src/cli.ts
+++ cli/src/cli.ts
@@ -36,7 +36,7 @@ type ServeOptions = {
 const program = new Command()
 program
   .name("metaliq")
-  .version("1.0.0-beta.92")
+  .version("1.0.0-beta.93")
 
 program
   .command("run [modelName]")
--- ui/media/src/pdf-viewer.ts
+++ ui/media/src/pdf-viewer.ts
@@ -4,14 +4,20 @@ import { guard } from "lit/directives/guard.js"
 import { fieldContainer } from "@metaliq/forms"
 import { defaultFileInputOptions, fileInput, FileInputOptions } from "./media"
 
-export const pdfViewer: MetaView<string> = (v, $) => html`
+export type Fetch = (url: RequestInfo, init?: RequestInit) => Promise<Response>
+
+export type PdfViewerOptions = {
+  fetch?: Fetch
+}
+
+export const pdfViewer = (options: PdfViewerOptions = {}): MetaView<string> => (v, $) => html`
   ${guard(v, () => {
     const id = Math.ceil(Math.random() * 999999)
     const viewerId = "mq-pdf-viewer-" + id.toString()
 
     if (v) {
       setTimeout(() => {
-        makeBlobUrl(v)
+        makeBlobUrl(v, options)
           .then(blobUrl => {
             const iframe: HTMLIFrameElement = document.querySelector(`#${viewerId}`)
             iframe.src = `node_modules/@metaliq/media/res/pdf-viewer/viewer.html?file=${blobUrl}`
@@ -28,7 +34,7 @@ export const pdfViewer: MetaView<string> = (v, $) => html`
   })}
 `
 
-export const pdfInputField = (options: FileInputOptions = {}) => {
+export const pdfInputField = (options: FileInputOptions & PdfViewerOptions = {}) => {
   options = {
     ...defaultFileInputOptions,
     accept: ".pdf",
@@ -37,14 +43,16 @@ export const pdfInputField = (options: FileInputOptions = {}) => {
   }
   return fieldContainer(options)((v, $) => [
     fileInput(options)(v, $),
-    v ? pdfViewer(v, $) : ""
+    v ? pdfViewer(options)(v, $) : ""
   ])
 }
 
 /**
  * Turn any URL (e.g. remote HTTP or Base64 encoded data URL) into a blob object URL.
  */
-export const makeBlobUrl = async (url: string) => {
+export const makeBlobUrl = async (url: string, options: PdfViewerOptions) => {
+  const fetch: Fetch = options.fetch || window.fetch.bind(window)
+
   if (url.match(/^blob/)) return url
   else {
     const response = await fetch(url)
