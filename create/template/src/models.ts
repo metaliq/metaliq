68072f08b917f81e77ffbbd86c45ff6153d94ef2
tstewart <tstewart@klaudhaus.com>
2025-01-19 20:50:26 +1100

    Derive GraphQL API URL from template lcoation HREF.

--- create/template/src/models.ts
+++ create/template/src/models.ts
@@ -89,7 +89,7 @@ export const appModel: MetaModel<App> = {
     nav: navModel,
     modal: modalModel
   },
-  init: () => initApi("http://localhost:8940/graphql", {
+  init: () => initApi(`http://${location.href}:8940/graphql`, {
     // Link the initialised API to a response handler that displays progress and errors
     onResponse: handleResponseErrors(showMessage, showProgress)
   }) && null,
